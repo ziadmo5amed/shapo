d696237560aa27a0fe4c535f07a45379e00af1cf
ziadmo5amed <161350029+ziadmo5amed@users.noreply.github.com>
2025-03-14 17:21:42 +0200

    section services

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const icons = document.querySelectorAll('.icon');
+icons.forEach(icon => {
+  icon.addEventListener('click', () => {
+    const userInput = prompt('أدخل ما تريد البحث عنه:');
+  });
+});
+
+
+ 
